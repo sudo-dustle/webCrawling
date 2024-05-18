3c6b979acb6055f6ec4508fefbcc295ae6410c39
ChangyhyeonP <parkchang12332@gmail.com>
2024-05-18 23:05:07 +0900

    data_element add

--- demo/noticelist.py
+++ demo/noticelist.py
@@ -1,3 +1,4 @@
+import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -7,8 +8,10 @@ def get_all_notices():
     # Chrome 드라이버 설정
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
+    today = datetime.date.today().strftime("%Y-%m-%d")
+
     # 원하는 URL 열기
-    driver.get('https://www.daegu.ac.kr/article/DG159/list')
+    driver.get('https://www.daegu.ac.kr/article/DG159/list?pageIndex=1&')
 
     # 모든 공지사항 요소 가져오기
     notice_elements = driver.find_elements(By.CSS_SELECTOR, '#sub_contents > div > table > tbody > tr')
@@ -17,13 +20,18 @@ def get_all_notices():
     all_notices = []
 
     # 각 공지사항 요소에서 제목과 링크 추출하여 리스트에 추가
-    for element in notice_elements:
+    for idx, element in enumerate(notice_elements, start=1):
+        if idx <= 12:  # 12번째 공지사항은 리스트에 추가하지 않음
+            continue
         title_element = element.find_element(By.CSS_SELECTOR, 'td.list_left > a')
+        date_element = element.find_element(By.CSS_SELECTOR, 'td:nth-child(3)')  # 공지사항의 날짜 요소 선택
         title = title_element.text.strip()
+        date = date_element.text.strip()
         link = title_element.get_attribute('href')
-        all_notices.append({'title': title, 'link': link})
 
-    # 브라우저 닫기
+        if date == today:
+            all_notices.append({'number': idx, 'title': title, 'link': link})    # 브라우저 닫기
+    
     driver.quit()
 
     return all_notices
