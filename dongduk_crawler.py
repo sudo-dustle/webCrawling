aa9a60c389a43a324f021f51ebd5c11ec808d230
sudo-dustle <asdf748596123@gmail.com>
2024-11-09 19:51:04 +0900

    feat: 새로운 홈페이지 크롤러 구현

--- dongduk_crawler.py
+++ dongduk_crawler.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 url = 'https://www.dongduk.ac.kr/www/contents/kor-noti.do?gotoMenuNo=kor-noti'  
 response = requests.get(url)    
 dongduk_url = 'https://www.dongduk.ac.kr/www/contents/kor-noti.do?schM=view&page=1&viewCount=10&id='
+idx = 88400
+
+
 
 if response.status_code == 200:
     html = response.text
@@ -11,16 +14,24 @@ if response.status_code == 200:
     notices = soup.select_one('ul.board-basic')
     elements = notices.select('li > dl')
 
+    set = set()
+
     for element in elements:
         id = element.a.get('onclick').split("'")[1] # onclick 속성 값 중 " ' " 로 split 해서 두번째 값 가져옴
         title = element.a.text.strip() 
         date = element.find_all('span', 'p_hide')[1].text # .find_all(태그 이름, 속성) 해당 하는 정보 모두 조회
+        
+        set.add((int(id), title, date))
 
-        print(date)
-        print(title)
-        print(dongduk_url + id)
-        print('--------------------------------')
+    list = [element for element in set if element[0] > idx]
 
+    list.sort(key = lambda x:x[0])
+    idx = list[-1][0]
 
-else : 
-    print(response.status_code)
+    noticeList = []
+    for element in list:
+        notice = '[' + element[2] + ']\n' + element[1] + '\n' + dongduk_url + str(element[0])
+        noticeList.append(notice)
+
+    
+    print(noticeList)
